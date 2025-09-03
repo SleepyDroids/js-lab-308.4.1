29d4e2d1d9f1dd94e225de42fc6fbfcf12e5bcac
SleepyDroids <jacquelinelwrites@gmail.com>
2025-09-03 18:52:21 -0400

    looped through the csv string successfully

--- 308.4.1-homework.js
+++ 308.4.1-homework.js
@@ -3,3 +3,18 @@
 // https://www.canva.com/design/DAFxumk-vR0/VYKb17-6iKqVOD5NOVsidA/edit
 
 // Part 1: Refactoring Old Code
+
+// storing the CSV data in a string
+let originalCSV = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+// creating a rows variable to split the string at the delimiter \n
+// let rows = originalCSV.split("\n");
+// // verifying it's splitting correctly
+// console.log(rows);
+
+// Loop through the characters of a given CSV string
+
+for (i = 0; i < originalCSV.length; i++) {
+    // logging out the entire index for the string
+    console.log(i);
+}
